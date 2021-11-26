c3a88b247af164cc30d281ec0b1a0a53c6f3c1a9
Tim Addison <git@tjaddison.com>
2021-11-26 15:22:23 -0500

    pull an item back and map insert/update

--- database-backup/src/index.ts
+++ database-backup/src/index.ts
@@ -1,13 +1,28 @@
-import { initializeApp, cert, ServiceAccount } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
+import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
+import { getFirestore, Timestamp } from "firebase-admin/firestore";
 
-import serviceAccount from './serviceAccount.json';
+import serviceAccount from "./serviceAccount.json";
 
 initializeApp({
   credential: cert(serviceAccount as ServiceAccount),
 });
 (async () => {
   const db = getFirestore();
-  const queryResult = await db.collection('transactions').limit(1).get();
-  console.log(queryResult.docs[0].data());
+  const queryResult = await db
+    .collection("transactions")
+    .orderBy("reportingDateTicks")
+    .limit(1)
+    .get();
+
+  const allItems = queryResult.docs.map((d) => <any>{ ...d.data(), id: d.id });
+
+  const finalItems = allItems.map((item) => {
+    return {
+      ...item,
+      insertedAtTicks: (item!.insertedAt as Timestamp).toMillis(),
+      updatedAtTicks: (item.updatedAt as Timestamp).toMillis(),
+    };
+  });
+
+  console.log(finalItems);
 })();
